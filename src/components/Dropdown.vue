bf11652a697dc6d5d985d7456abd3e82f86b8cba
salomoneb <salomone.l.baquis@gmail.com>
2017-09-22 18:09:26 -0400

    kinda works?

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -4,66 +4,33 @@
 			<option disabled value="">Select a Chamber</option>
 			<option>House</option>
 			<option>Senate</option>
-		</select>
-		<select class="committee-dropdown" v-if="chamber" v-model="committee">
+		</select> 
+		<img v-if="chamber && !committees.length" src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif">
+		<select class="committee-dropdown" v-if="chamber && committees.length" v-model="committee">
 			<option disabled value="">Select a Committee</option>
 			<option v-for="committee in committees" :value="committee.api_uri"> {{ committee.name }}</option>
-			<!-- <option :value="chamber">{{ getCommitteeList() }}</option> -->
-			<!-- <option v-for="post in posts" :value="post"> {{ parseResults() }} works as an </option> -->
 		</select>
-		<results :lawmakerData="committee"></results>
-<!-- 		<table>
-			<tr v-for="committee in committees"><td>{{committee.userId}}</td><td>{{committee.body}}</td></tr>
-		</table> -->
-		<!-- <results :lawmakerData="committee"></results> -->
+		<results v-if="committee" :committeeUrl="committee"></results>
 	</div>
 </template>
 
 <script>
-	import Results from './Results'
-	import axios from 'axios';
-  var lawmakers = [
-    {name: "tom", occupations: [
-      { 
-        job: "accountant", 
-        hobby: "lawyer"
-      }  
-    ]},
-    {name: "dick", occupations: [
-      { 
-        job: "doctor", 
-        hobby: "businessman"
-      }  
-    ]},
-    {name: "harry"}
-  ]	
+	import Results from "./Results"
+	import axios from "axios"
+
 	export default {
 		name: "dropdown", 
 		data: function() {
 			return {
-				chamber: false,
 				committee: "",
-				committees: [],
-				errors: [],
 				chamber: "",
-				lawmakers: lawmakers
+				committees: [],
+				errors: []
 			}
 		},
-		// created: function() {
-
-		// }, 
 		methods: {
-			parseResults: function() {
-				var item = this.posts
-				var results = item.results
-				console.log(results)
-			}, 
-			getChamber: function() {
-				return this.chamber
-			}, 
 			getCommitteeList: function() {
 				if (this.chamber != "") {
-					console.log(`https://api.propublica.org/congress/v1/115/${this.chamber.toLowerCase()}/committees.json`)
 					return axios.request("GET", {
 						url: `https://api.propublica.org/congress/v1/115/${this.chamber.toLowerCase()}/committees.json`, 
 						headers: {
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,22 +1,40 @@
 <template>
-	<div class="results">
-		<h3> {{ returnLawmakers() }} </h3>
+	<div class="results" v-if="committeeUrl" @change="committeeMembers">
+		<ul>
+			<li v-for="member in members">{{ member.name }}</li>
+		</ul>
 	</div>
 </template>
-
+ 
 <script>
-	import axios from 'axios';
-
+	import axios from "axios"
 	// Add API call to the committee endpoint, add table to this template where committee data will be displayed
 	export default {
 		name: "results", 
-		props: ["lawmakerData"],
-		methods: {
-			returnLawmakers: function() {
-				var item = this.lawmakerData
-				console.log(this.lawmakerData)
-				return item
+		props: ["committeeUrl"], 
+		data: function() {
+			return {
+				members: [], 
+				errors: []
 			}
-		}
+		}, 
+		computed: {
+			committeeMembers: function() {
+				return axios.request("GET", {
+					url: this.committeeUrl, 
+					headers: {
+						"X-API-Key": "2eX2Dxe43hYuiIoXVeBg463BY8rpXVfY1PzbIUBu"
+					}
+				})
+				.then(response => {
+		      // JSON responses are automatically parsed.
+		      this.members = response.data.results[0].current_members
+		      console.log(this.members)
+		    })
+		    .catch(e => {
+		      this.errors.push(e)
+		    })	
+			}
+		}	
 	}
 </script>
